9e40aa61af8404544d640a14f4ea7c7e48239662
flapan <mikkel@gadegaard.eu>
2023-04-21 14:22:57 -0700

    Custom type with interface, and extending another interface optionally or Partial as it is defined in TypeScript

--- src/demo.ts
+++ src/demo.ts
@@ -1,5 +1,19 @@
-class Contact {
-    id;
-    name;
-    birthDate;
+interface Contact extends Partial<Address>{
+    id: number;
+    name: string;
+    birthDate?: Date;
 }
+
+interface Address {
+    line1: string;
+    line2: string;
+    state: string;
+    postalcode: string;
+}
+
+let primaryContact: Contact = {
+    birthDate: new Date("20-12-2018"),
+    id: 4,
+    name: "Hugo Schiller"
+};
+
